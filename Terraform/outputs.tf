5b2135e7630c7b813ba8b52d98e6df13b9277fab
sirishap03 <ponnadasirisha01@gmail.com>
2025-05-14 12:51:40 +0530

    Create outputs.tf

---
+++ Terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "instance_group_name" {
+  description = "The name of the managed instance group"
+  value       = google_compute_region_instance_group_manager.default.name
+}
+
+output "backend_service_name" {
+  description = "The backend service used for load balancing"
+  value       = google_compute_backend_service.default.name
+}
+
+output "load_balancer_ip" {
+  description = "The external IP address of the Load Balancer"
+  value       = google_compute_global_forwarding_rule.default.ip_address
+}
