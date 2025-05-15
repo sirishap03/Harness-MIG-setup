6dd1fdf81ab5dd2066843bdc1d40b9f0e13e6d8f
sirishap03 <ponnadasirisha01@gmail.com>
2025-05-15 17:36:32 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -29,6 +29,10 @@ resource "google_compute_instance_template" "default" {
   metadata = {
     ssh-keys = "ansible:${tls_private_key.my_ssh_key.public_key_openssh}"
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "google_compute_health_check" "default" {
