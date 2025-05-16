98b56f627ac14aeaabd7b4578ee5ad5118dfdfe8
sirishap03 <ponnadasirisha01@gmail.com>
2025-05-16 11:23:51 +0530

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,18 +1,64 @@
-output "instance_group" {
-  value = google_compute_region_instance_group_manager.default.instance_group
+output "project_id" {
+  description = "The GCP project ID"
+  value       = var.project_id
 }
 
-output "ssh_private_key_pem" {
-  value     = tls_private_key.my_ssh_key.private_key_pem
-  sensitive = true
+output "region" {
+  description = "The region where resources are deployed"
+  value       = var.region
 }
 
-output "ssh_public_key_openssh" {
-  value = tls_private_key.my_ssh_key.public_key_openssh
+output "zone" {
+  description = "The zone where instances are located"
+  value       = var.zone
+}
+
+output "instance_template" {
+  description = "The name of the instance template used in the MIG"
+  value       = google_compute_instance_template.default.name
+}
+
+output "mig_name" {
+  description = "The name of the managed instance group"
+  value       = google_compute_instance_group_manager.default.name
+}
+
+output "autoscaler_status" {
+  description = "The name of the autoscaler linked to the MIG"
+  value       = google_compute_autoscaler.default.name
+}
+
+output "backend_service" {
+  description = "The name of the backend service used by the load balancer"
+  value       = google_compute_backend_service.default.name
+}
+
+output "health_check" {
+  description = "The health check configured for load balancer"
+  value       = google_compute_health_check.http.name
+}
+
+output "url_map" {
+  description = "The URL map used by the load balancer"
+  value       = google_compute_url_map.default.name
+}
+
+output "http_proxy" {
+  description = "The target HTTP proxy for the load balancer"
+  value       = google_compute_target_http_proxy.default.name
+}
+
+output "forwarding_rule" {
+  description = "The forwarding rule for the load balancer"
+  value       = google_compute_global_forwarding_rule.default.name
 }
 
 output "load_balancer_ip" {
-  value = google_compute_global_forwarding_rule.default.ip_address
+  description = "The external IP of the global HTTP load balancer"
+  value       = google_compute_global_forwarding_rule.default.ip_address
 }
 
-
+output "load_balancer_url" {
+  description = "URL to access Apache web server via load balancer"
+  value       = "http://${google_compute_global_forwarding_rule.default.ip_address}"
+}
