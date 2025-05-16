ee89a77c5b720000dbb2900a90ce854b90924920
sirishap03 <ponnadasirisha01@gmail.com>
2025-05-16 18:14:35 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,14 +1,48 @@
+# GCP Project ID
 variable "project_id" {
-  description = "GCP Project ID"
+  description = "The GCP project ID"
   type        = string
+  default     = "symbolic-pipe-457709-n9"
 }
 
+# GCP Region
 variable "region" {
-  description = "GCP region"
+  description = "The GCP region to deploy resources"
   type        = string
+  default     = "us-central1"
 }
 
+# GCP Zone
 variable "zone" {
-  description = "GCP zone"
+  description = "The GCP zone to deploy instances"
   type        = string
+  default     = "us-central1-a"
+}
+
+# Machine Type
+variable "machine_type" {
+  description = "GCP Compute Engine machine type"
+  type        = string
+  default     = "e2-medium"
+}
+
+# Instance Template Name Prefix
+variable "instance_template_prefix" {
+  description = "Prefix for the instance template name"
+  type        = string
+  default     = "apache-template"
+}
+
+# Base Instance Name
+variable "base_instance_name" {
+  description = "Base name for VM instances in the group"
+  type        = string
+  default     = "apache-instance"
+}
+
+# Target Size of MIG
+variable "target_size" {
+  description = "Initial number of instances in MIG"
+  type        = number
+  default     = 2
 }
