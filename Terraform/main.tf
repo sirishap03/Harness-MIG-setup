34bc334a5d3c446fc82744bded5a1633c8784dab
sirishap03 <ponnadasirisha01@gmail.com>
2025-05-16 18:58:48 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,16 @@
 # main.tf
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+}
 provider "google" {
   project = var.project_id
   region  = var.region
