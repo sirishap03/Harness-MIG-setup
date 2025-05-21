dbd5cf2bf12573392875847d174e084632e7934e
sirishap03 <ponnadasirisha01@gmail.com>
2025-05-21 13:13:05 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,22 +1,20 @@
+# main.tf
 provider "google" {
   project = var.project_id
   region  = var.region
+  zone    = var.zone
 }
 
-resource "google_compute_instance_template" "apache_template" {
-  name_prefix  = "apache-template"
-  machine_type = "e2-micro"
+resource "google_compute_instance_template" "default" {
+  name_prefix = "apache-template"
+  machine_type = "e2-medium"
 
   tags = ["http-server"]
 
-  metadata = {
-    enable-oslogin = "TRUE"
-  }
-
   disk {
+    source_image = "debian-cloud/debian-11"
     auto_delete  = true
     boot         = true
-    source_image = "debian-cloud/debian-11"
   }
 
   network_interface {
@@ -25,28 +23,42 @@ resource "google_compute_instance_template" "apache_template" {
   }
 
   metadata_startup_script = <<-EOT
-    #!/bin/bash
-    apt update
-    apt install -y apache2
-    systemctl enable apache2
-    systemctl start apache2
+    sudo apt update
+    sudo apt install -y apache2
+    sudo systemctl enable apache2
+    sudo systemctl start apache2
   EOT
 }
 
-resource "google_compute_region_instance_group_manager" "apache_group" {
-  name               = "apache-group"
+resource "google_compute_region_instance_group_manager" "default" {
+  name               = "apache-mig"
+  base_instance_name = "apache-instance"
   region             = var.region
-  base_instance_name = "apache"
   version {
-    instance_template = google_compute_instance_template.apache_template.id
+    instance_template = google_compute_instance_template.default.id
   }
-  target_size = 2
+
+  target_size = 2  # Start with 2 instances
+  auto_healing_policies {
+    health_check      = google_compute_health_check.default.id
+    initial_delay_sec = 300
+  }
 }
 
-resource "google_compute_autoscaler" "apache_autoscaler" {
-  name   = "apache-autoscaler"
+resource "google_compute_health_check" "default" {
+  name               = "apache-health-check"
+  check_interval_sec = 10
+  timeout_sec        = 5
+  http_health_check {
+    port = 80
+  }
+}
+
+resource "google_compute_region_autoscaler" "default" {
+  name   = "mig-autoscaler"
   region = var.region
-  target = google_compute_region_instance_group_manager.apache_group.id
+
+  target = google_compute_region_instance_group_manager.default.id
 
   autoscaling_policy {
     max_replicas    = 5
@@ -59,40 +71,38 @@ resource "google_compute_autoscaler" "apache_autoscaler" {
   }
 }
 
-resource "google_compute_health_check" "http" {
-  name = "http-health-check"
-  http_health_check {
-    port = 80
-  }
-}
 
-resource "google_compute_backend_service" "apache_backend" {
-  name          = "apache-backend"
-  protocol      = "HTTP"
-  port_name     = "http"
-  health_checks = [google_compute_health_check.http.id]
+resource "google_compute_backend_service" "default" {
+  name                            = "apache-backend-service"
+  protocol                        = "HTTP"
+  port_name                       = "http"
+  timeout_sec                     = 10
+  health_checks                   = [google_compute_health_check.default.id]
+  load_balancing_scheme           = "EXTERNAL"
+  connection_draining_timeout_sec = 0
+
   backend {
-    group = google_compute_region_instance_group_manager.apache_group.instance_group
+    group = google_compute_region_instance_group_manager.default.instance_group
   }
+
+  depends_on = [google_compute_health_check.default]
 }
 
-resource "google_compute_url_map" "apache_map" {
-  name            = "apache-map"
-  default_service = google_compute_backend_service.apache_backend.id
+
+resource "google_compute_url_map" "default" {
+  name            = "apache-url-map"
+  default_service = google_compute_backend_service.default.id
 }
 
-resource "google_compute_target_http_proxy" "apache_proxy" {
-  name   = "apache-proxy"
-  url_map = google_compute_url_map.apache_map.id
+resource "google_compute_target_http_proxy" "default" {
+  name   = "apache-http-proxy"
+  url_map = google_compute_url_map.default.id
 }
 
-resource "google_compute_global_address" "apache_ip" {
-  name = "apache-ip"
-}
-
-resource "google_compute_global_forwarding_rule" "apache_forwarding" {
-  name        = "apache-forwarding"
-  ip_address  = google_compute_global_address.apache_ip.address
-  target      = google_compute_target_http_proxy.apache_proxy.id
-  port_range  = "80"
+resource "google_compute_global_forwarding_rule" "default" {
+  name                  = "apache-forwarding-rule"
+  target                = google_compute_target_http_proxy.default.id
+  port_range            = "80"
+  load_balancing_scheme = "EXTERNAL"
+  ip_protocol           = "TCP"
 }
